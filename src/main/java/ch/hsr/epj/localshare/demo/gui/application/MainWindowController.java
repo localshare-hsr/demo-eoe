e88548462d440ff0e8fb5bb0db69ed531f65e775
Dominique <illi.dominique.v@hsr.ch>
2019-03-27 20:46:07 +0100

    Changed some GUI elements

--- src/main/java/ch/hsr/epj/localshare/demo/gui/application/MainWindowController.java
+++ src/main/java/ch/hsr/epj/localshare/demo/gui/application/MainWindowController.java
@@ -2,20 +2,46 @@ package ch.hsr.epj.localshare.demo.gui.application;
 
 import ch.hsr.epj.localshare.demo.gui.data.Peer;
 import ch.hsr.epj.localshare.demo.logic.DiscoveryController;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class MainWindowController implements Initializable, PeerUpdaterIF {
 	
 	@FXML
+	private AnchorPane preferencesRootPane;
+	@FXML
 	private ListView<Peer> listView;
-	
+	@FXML
 	private ObservableList<Peer> peerObservableList;
+
+	@FXML
+	private void handlePreferencesButtonAction(ActionEvent event) throws IOException {
+		//Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+//		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//		alert.setTitle("Information Dialog");
+//		alert.setHeaderText("Look, an Information Dialog");
+//		alert.setContentText("I have a great message for you!");
+//		alert.showAndWait();
+
+		preferencesRootPane = null;
+		AnchorPane preferencesPane = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/PreferencesView.fxml"));
+		preferencesRootPane.getChildren().setAll(preferencesPane);
+	}
 	
 	public MainWindowController() {
 		
--- src/main/java/ch/hsr/epj/localshare/demo/gui/data/PeerManager.java
+++ src/main/java/ch/hsr/epj/localshare/demo/gui/data/PeerManager.java
@@ -2,23 +2,30 @@ package ch.hsr.epj.localshare.demo.gui.data;
 
 import java.util.ArrayList;
 
+import ch.hsr.epj.localshare.demo.network.Discovery;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class PeerManager {
-	private int id;
 	private ArrayList<Peer> peerList;
-	
 
-	public PeerManager(int id) {
-		this.id = id;
+	public PeerManager() {
 		peerList = new ArrayList<Peer>();
+	}
+
+	public void createPeers(){
+		Discovery discovery = new Discovery();
+		String[] ip_array = discovery.getIPAsArray();
+
+		for(String ip : ip_array){
+			Peer p = new Peer(ip, "");
+			addPeer(p);
+		}
 	}
 	
 	public void addPeer(Peer p) {
 		peerList.add(p);
 	}
-	
 	public ArrayList<Peer> getList(){
 		return peerList;
 	}
