2cf802aef1d4211ceb8ac25326ff2304de573fe6
Marco Zollinger <marco@freelabs.space>
2019-04-18 13:46:52 +0200

    add Transfer class, stubs in HttpClientController

--- src/main/java/ch/hsr/epj/localshare/demo/logic/HttpClientController.java
+++ src/main/java/ch/hsr/epj/localshare/demo/logic/HttpClientController.java
@@ -2,4 +2,11 @@ package ch.hsr.epj.localshare.demo.logic;
 
 public class HttpClientController {
 
+  public void sendNotification(Transfer transfer) {
+    // stub
+  }
+
+  public void getMetadataFromPeer(Transfer transfer) {
+    // stub
+  }
 }
