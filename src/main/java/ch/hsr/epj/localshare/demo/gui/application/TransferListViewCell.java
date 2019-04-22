dea479234d914fd53d0413092592e6464ca9de50
Dominique <dominique.illi@ins.hsr.ch>
2019-04-22 23:47:50 +0200

    implemented methods for buttons in transferCell

--- src/main/java/ch/hsr/epj/localshare/demo/gui/application/TransferListViewCell.java
+++ src/main/java/ch/hsr/epj/localshare/demo/gui/application/TransferListViewCell.java
@@ -4,8 +4,10 @@ import ch.hsr.epj.localshare.demo.gui.data.Transfer;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.GridPane;
 
 public class TransferListViewCell extends ListCell<Transfer> {
@@ -18,6 +20,18 @@ public class TransferListViewCell extends ListCell<Transfer> {
 
   @FXML
   Label filename;
+
+  @FXML
+  Button buttonAccept;
+
+  @FXML
+  Button buttonDecline;
+
+  @FXML
+  Button buttonCancelTransfer;
+
+  @FXML
+  ProgressBar transferProgressBar;
 
   private FXMLLoader mLLoader;
 
@@ -40,6 +54,35 @@ public class TransferListViewCell extends ListCell<Transfer> {
           e.printStackTrace();
         }
       }
+
+      buttonAccept.setOnAction(
+          event -> {
+            gridPaneTransfer.setStyle("-fx-background-color: PALEGREEN");
+            buttonAccept.setVisible(false);
+            buttonAccept.setDisable(true);
+            buttonDecline.setVisible(false);
+            buttonDecline.setDisable(true);
+            transferProgressBar.setVisible(true);
+            buttonCancelTransfer.setDisable(false);
+            buttonCancelTransfer.setVisible(true);
+
+            //handle ProgressBar e.g if 10% of File loaded
+          }
+      );
+
+      buttonCancelTransfer.setOnAction(
+          event -> {
+            //cancel transfer thread + delete view
+
+          }
+      );
+
+      buttonDecline.setOnAction(
+          event -> {
+            // Methode im MainWindowController aufrufen mittels Interface das aktuell selektiertes Objekt aus ListView + ObservableList löscht
+
+          }
+      );
 
       size.setText(String.valueOf(transfer.getSize()));
       filename.setText(String.valueOf(transfer.getName()));
