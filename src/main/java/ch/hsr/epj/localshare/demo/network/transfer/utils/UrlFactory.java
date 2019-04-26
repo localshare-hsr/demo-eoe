b1b84cc55bb7543c7ef2b572a5ba6dd75fbce794
Pascal Knecht <pascal.knecht@hsr.ch>
2019-04-26 16:23:23 +0200

    refactor class and apply sonar feedback

--- src/main/java/ch/hsr/epj/localshare/demo/network/transfer/utils/UrlFactory.java
+++ src/main/java/ch/hsr/epj/localshare/demo/network/transfer/utils/UrlFactory.java
@@ -1,25 +1,36 @@
 package ch.hsr.epj.localshare.demo.network.transfer.utils;
 
 import ch.hsr.epj.localshare.demo.logic.Transfer;
+import ch.hsr.epj.localshare.demo.network.utils.IPAddressUtil;
+import java.io.File;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class UrlFactory {
 
+  private static final String PROTCOL = "http://";
+  private static final String PORT = "8640";
+
   private UrlFactory() {
   }
 
-  public static URL generateUrl(InetAddress ipAddress) throws MalformedURLException {
-    return new URL("http://" + ipAddress.toString() + "/");
+  public static URL generateNotifyUrl(final InetAddress ipAddress) throws MalformedURLException {
+    return new URL(PROTCOL + ipAddress.getHostAddress() + ":" + PORT + "/notify/");
   }
 
-  public static URL generateNotifyUrl(InetAddress ipAddress) throws MalformedURLException {
-    return new URL("http://" + ipAddress.getHostAddress() + ":8640/notify/");
+  public static URL generateMetaDataUrl(final Transfer transfer) throws MalformedURLException {
+    String ip = getIPAddress(transfer);
+    return new URL(PROTCOL + ip + ":" + PORT + "/share/" + transfer.getFileUri() + "/");
   }
 
-  public static URL generateMetaDataUrl(Transfer transfer) throws MalformedURLException {
-    String ip = transfer.getPeerAddress().getHostAddress();
-    return new URL("http://" + ip + ":8640/share/" + transfer.getFileUri() + "/");
+  public static String generateDownloadableURL(final String randomToken, File file) {
+    return PROTCOL + IPAddressUtil.getLocalIPAddress().getHostAddress() + ":" + PORT + "/share/"
+        + randomToken + "/"
+        + file.getName();
+  }
+
+  private static String getIPAddress(final Transfer transfer) {
+    return transfer.getPeerAddress().getHostAddress();
   }
 }
