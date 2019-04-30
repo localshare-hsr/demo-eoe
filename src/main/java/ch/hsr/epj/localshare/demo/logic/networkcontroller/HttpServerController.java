91c4eab4989dc31044611d46ad9046cb26ac4015
Pascal Knecht <pascal.knecht@hsr.ch>
2019-04-30 22:06:05 +0200

    rewrite progressbar feature
    - improve stability
    - add speed capabilities (bps) also in UI
    - add estimated time to finish capability (seconds) also in UI
    - add unit tests
    - remove server upload capability due to compatibility issues

--- src/main/java/ch/hsr/epj/localshare/demo/logic/networkcontroller/HttpServerController.java
+++ src/main/java/ch/hsr/epj/localshare/demo/logic/networkcontroller/HttpServerController.java
@@ -1,17 +1,14 @@
 package ch.hsr.epj.localshare.demo.logic.networkcontroller;
 
 import ch.hsr.epj.localshare.demo.logic.Transfer;
-import ch.hsr.epj.localshare.demo.network.transfer.HTTPProgress;
 import ch.hsr.epj.localshare.demo.network.transfer.server.HTTPServer;
 import java.io.File;
 import java.net.InetAddress;
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
 
-public class HttpServerController implements Observer {
+public class HttpServerController {
 
   private HTTPServer httpServer;
   private HttpClientController httpClientController;
@@ -41,18 +38,9 @@ public class HttpServerController implements Observer {
     byte[] key = new byte[16];
     secureRandom.nextBytes(key);
     String privatePath = new String(Base64.getUrlEncoder().encode(key));
-    HTTPProgress httpProgress = httpServer.createNewShare(privatePath, files);
-    httpProgress.addObserver(this);
+    httpServer.createNewShare(privatePath, files);
     Transfer transfer = new Transfer(peer, privatePath);
     httpClientController.sendNotification(transfer);
-  }
-
-  @Override
-  public void update(Observable o, Object arg) {
-    int percent = (int) arg;
-    if (percent % 20 == 0) {
-      System.out.println("Upload completeness = " + percent + "%");
-    }
   }
 
   public void receivedNotification(Transfer transfer) {
