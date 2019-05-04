a73e9476c69ce1f7baf3113e278a9496c647666e
Pascal Knecht <pascal.knecht@hsr.ch>
2019-05-04 12:31:01 +0200

    add meaningful time and transfer speed formatting

--- src/main/java/ch/hsr/epj/localshare/demo/logic/networkcontroller/FileTransfer.java
+++ src/main/java/ch/hsr/epj/localshare/demo/logic/networkcontroller/FileTransfer.java
@@ -2,6 +2,7 @@ package ch.hsr.epj.localshare.demo.logic.networkcontroller;
 
 import ch.hsr.epj.localshare.demo.gui.presentation.Peer;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 
@@ -35,12 +36,33 @@ public class FileTransfer {
     progress.setProgress(percentage);
   }
 
-  public void updateTransferSpeed(final long bps) {
-    transferSpeedInBytesPerSecond.setText(bps + " Bps");
+  public void updateTransferSpeed(final int bps) {
+    String niceFormat;
+
+    if (bps > 1_000_000_000) {
+      niceFormat = (bps / 1_000_000_000) + " GB/s";
+    } else if (bps > 1_000_000) {
+      niceFormat = (bps / 1_000_000) + " MB/s";
+    } else if (bps > 1_000) {
+      niceFormat = (bps / 1_000) + " kB/s";
+    } else {
+      niceFormat = bps + " B/s";
+    }
+
+    transferSpeedInBytesPerSecond.setText(niceFormat);
   }
 
-  public void updateTimeToGo(final long seconds) {
-    approximateTimeToDownloadInSeconds.setText(seconds + " s");
+  public void updateTimeToGo(final long millis) {
+    long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+    long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+    String niceFormat;
+    if (minutes == 0) {
+      niceFormat = String.format("%2d sec", seconds - TimeUnit.MINUTES.toSeconds(minutes));
+    } else {
+      niceFormat = String
+          .format("%d min %2d sec", minutes, seconds - TimeUnit.MINUTES.toSeconds(minutes));
+    }
+    approximateTimeToDownloadInSeconds.setText(niceFormat);
   }
 
 }
