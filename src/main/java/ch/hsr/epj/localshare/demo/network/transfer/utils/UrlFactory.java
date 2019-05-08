ccb90bea37b71e6a0138a4c87c5467854e8a0deb
Marco Zollinger <marco@freelabs.space>
2019-05-08 11:31:55 +0200

    URL encode the file names

--- src/main/java/ch/hsr/epj/localshare/demo/network/transfer/utils/UrlFactory.java
+++ src/main/java/ch/hsr/epj/localshare/demo/network/transfer/utils/UrlFactory.java
@@ -3,9 +3,11 @@ package ch.hsr.epj.localshare.demo.network.transfer.utils;
 import ch.hsr.epj.localshare.demo.logic.Transfer;
 import ch.hsr.epj.localshare.demo.network.utils.IPAddressUtil;
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class UrlFactory {
 
@@ -24,10 +26,11 @@ public class UrlFactory {
     return new URL(PROTOCOL + ip + ":" + PORT + "/share/" + transfer.getFileUri() + "/");
   }
 
-  public static String generateDownloadableURL(final String randomToken, File file) {
+  public static String generateDownloadableURL(final String randomToken, File file)
+      throws UnsupportedEncodingException {
     return PROTOCOL + IPAddressUtil.getLocalIPAddress().getHostAddress() + ":" + PORT + "/share/"
         + randomToken + "/"
-        + file.getName();
+        + URLEncoder.encode(file.getName(), "UTF-8");
   }
 
   private static String getIPAddress(final Transfer transfer) {
